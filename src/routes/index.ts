8a6f9e00aef9ac68d19e858e4866d111c4ed7544
Tiberio Brasil <tiberiobrasil@gmail.com>
2020-04-16 15:03:27 -0300

    Iniciando back-end do app

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,13 @@
 import { Router } from 'express';
 
+import appointmentsRouter from './appointments.routes';
+import usersRouter from './users.routes';
+import sessionsRouter from './sessions.routes';
+
 const routes = Router();
 
-routes.post('/users', (req, res) => {
-  const { name, email } = req.body;
-
-  const user = {
-    name,
-    email,
-  };
-
-  return res.json(user);
-});
+routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
